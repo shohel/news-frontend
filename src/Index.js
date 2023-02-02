52f2c4005d41bcfea22ec703ef73f291b8ae0065
Shohel <mhshohel89@gmail.com>
2023-02-03 00:39:28 +0600

    Fixed eslint

--- src/Index.js
+++ src/Index.js
@@ -1,14 +1,12 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
-import '../assets/css/tailwindbase.scss';
-import 'react-loading-skeleton/dist/skeleton.css'
+import ReactDOM from "react-dom/client";
+import "../assets/css/tailwindbase.scss";
+import "react-loading-skeleton/dist/skeleton.css";
 import AppRoutes from "./core/AppRoutes";
-import {
-    RouterProvider,
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("newsRoot")).render(
-    <React.StrictMode>
-        <RouterProvider router={AppRoutes} />
-    </React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={AppRoutes} />
+  </React.StrictMode>
 );
--- src/core/Elements/Header.jsx
+++ src/core/Elements/Header.jsx
@@ -108,8 +108,12 @@ const Header = () => {
                                                     <a
                                                         href="#"
                                                         className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                        onClick={ (e) => {
+                                                            e.preventDefault();
+                                                            console.log('Hello');
+                                                        }}
                                                     >
-                                                        Settings
+                                                        Preferences
                                                     </a>
                                                 )}
                                             </Menu.Item>
